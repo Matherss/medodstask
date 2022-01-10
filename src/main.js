11935b3938b1104b5493b6aeef26496a2bf3ba29
Matherss <mathers_beats@mail.ru>
2022-01-10 20:03:10 +0300

    v2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
-
-const app = createApp(App);
-app.mount("#app");
-app.config.devtools = true;
+Vue.config.devtools = true;
+createApp(App).mount("#app");
